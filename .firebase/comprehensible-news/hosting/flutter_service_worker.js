9e463e00a3aed45f218a23f0d2cdf7fb8326a400
Paz <dancing.star@hotmail.co.uk>
2024-11-24 20:45:17 +0100

    Refactor article detail screen and content display; enhance UI with CustomScrollView and improved layout

--- .firebase/comprehensible-news/hosting/flutter_service_worker.js
+++ .firebase/comprehensible-news/hosting/flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "8e1e92950edd16ca776ea6b5f7ce78c6",
+"flutter_bootstrap.js": "6f7342d68d630449ad35970c08eac650",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "189de67813ecf50a2c69a1851f15c3c5",
 "/": "189de67813ecf50a2c69a1851f15c3c5",
-"main.dart.js": "90d287e9f3144d96c8c39f4aa76d068f",
+"main.dart.js": "9374a466af01b46d79848a84bc5be0c6",
 "manifest.json": "3aa4ac3a10dd358f305049eb49dc013f",
 "version.json": "e4612168cbcd7cd178a0a5a970b6fde5"};
 // The application shell files that are downloaded before a service worker can
